5d997c0bdeb29960a4c217e02c3160b1eba1ec2f
Mateus Silva <mateus.silva@geotech4web.com.br>
2020-03-22 12:22:41 -0300

    criando a aplicação em backend

---
+++ Backend/src/main/java/br/com/ranca/RancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RancaApplication.class, args);
+	}
+
+}
